79e7fd4f9b1c80dd33fb986d0c66315a17f41d66
djzhouqinyao <zhouqinyao@imdada.cn>
2019-04-25 14:48:25 +0800

    new hongya

---
+++ ComponentDemo/business/hongya/src/main/java/com/qinyaoz/hongya/ui/activity/IndexActivity.java
@@ -0,0 +1,15 @@
+package com.qinyaoz.hongya.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qinyaoz.hongya.R;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hy_activity_index);
+    }
+}
